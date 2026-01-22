8c42de46672d6bfeb224df477909725ec57dad05
Marcin Ziółkowski <62351083+MA3CIN@users.noreply.github.com>
2026-01-22 18:25:54 +0100

    valid anagram

--- LeetCode/242_Valid_Anagram.py
+++ LeetCode/242_Valid_Anagram.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if (len(s)!=len(t)):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
             return False
-        # dict for counters
-        s_dict,t_dict = {}, {}
-        for i in range(len(t)):
-            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
-            t_dict[t[i]] = 1 + t_dict.get(t[i], 0)
-        return s_dict == t_dict
+        
+        s_hashmap, t_hashmap = {}, {}    # letter -> count
+        for i in range(len(s)):
+                s_hashmap[s[i]] = s_hashmap.get(s[i],0) + 1
+                t_hashmap[t[i]] = t_hashmap.get(t[i],0) + 1
+        return s_hashmap == t_hashmap 
